44412becd7fb829476bdc53a0d0f02dd3dc7fbc2
dpill <a20141105059@mac05.local>
2016-06-20 11:05:20 +0800

    通过视频学习和问人，了解了循环语句一般形式为： for（单次表达式;条件表达式;末尾循环体） { 中间循环体； } 需要注意的是有没有空格是两种情况。

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  2
+//
+//  Created by 20141105059c on 16/6/20.
+//  Copyright © 2016年 20141105059c. All rights reserved.
+//
+
+import Foundation
+var a=1
+var i=1
+while i<100{
+i++
+a=a+i
+}
+print(a)
